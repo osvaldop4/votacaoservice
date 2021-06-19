3efc24f6a5cbcdd1fddac7d2c42b9db50b47d7f4
Osvaldo Pereira Duarte Junior <osvaldop4@gmail.com>
2021-06-18 21:53:39 -0300

    Ajustes na classe VotacaoApiApplication.java

--- src/main/java/com/example/votacaoapi/ServletInitializer.java
+++ src/main/java/com/example/votacaoapi/ServletInitializer.java
@@ -3,6 +3,7 @@ package com.example.votacaoapi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
--- src/main/java/com/example/votacaoapi/VotacaoApiApplication.java
+++ src/main/java/com/example/votacaoapi/VotacaoApiApplication.java
@@ -3,11 +3,12 @@ package com.example.votacaoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class VotacaoApiApplication {
+public class VotacaoApiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotacaoApiApplication.class, args);
 	}
-
+	
 }
